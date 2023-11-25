dd1752b80f7a5604c11f093e3a16a6cfeaa38325
Prince Igwe <igwep297@gmail.com>
2023-11-25 18:28:12 +0100

    started users module

---
+++ src/users/users.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { User, UserSchema } from './user.schema'
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      {name: User.name, schema: UserSchema}
+    ])
+  ]
+})
+export class UsersModule {}
