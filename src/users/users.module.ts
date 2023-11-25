32997444e77e2ee75279a4d2af347f345bc65888
Prince Igwe <igwep297@gmail.com>
2023-11-25 18:54:37 +0100

    service to create user on auth registration

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { User, UserSchema } from './user.schema'
 import { MongooseModule } from '@nestjs/mongoose';
+import { UsersService } from './users.service';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       {name: User.name, schema: UserSchema}
     ])
-  ]
+  ],
+  providers: [UsersService],
 })
 export class UsersModule {}
---
+++ src/users/users.service.ts
@@ -0,0 +1,23 @@
+import { Injectable, HttpException, HttpStatus, NotFoundException } from '@nestjs/common';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { User, UserDocument } from './user.schema';
+
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectModel(User.name) private userModel: Model<UserDocument>
+  ) { }
+  
+
+  async createUser(first_name: string, last_name: string, email: string, username: string, password: string) {
+    let existing_user = await this.userModel.findOne({ email: email }).exec() || await this.userModel.findOne({ username: username }).exec()
+    if (existing_user) {
+      throw new HttpException( "User with email/username already exists", HttpStatus.BAD_REQUEST )
+    }
+    const user = await this.userModel.create({ first_name: first_name, last_name: last_name, email: email, username: username, password: password })
+    user.save()
+    return user
+  }
+}
