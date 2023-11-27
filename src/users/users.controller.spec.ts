f7f836d4368d7dc446157a2260cba066929295f7
Prince Igwe <igwep297@gmail.com>
2023-11-27 11:19:27 +0100

    test for current logged in user profile

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,12 +1,45 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { User } from './user.schema';
+import {Request} from 'express'
+
 
 describe('UsersController', () => {
   let controller: UsersController;
 
+  let mockUsersService = {
+
+    myProfile: jest.fn((user: User) => {
+      user = {
+        first_name : 'test',
+        last_name : 'user3',
+        email : 'testuser3@gmail.com',
+        username : 'testuser3',
+        password : 'testpass123'
+      }
+      return {
+        "message": "My Profile",
+        "data": {
+          "first_name": "test",
+          "last_name": "user3",
+          "email": "testuser3@gmail.com",
+          "username": "testuser3"
+      }
+    }
+    } )
+
+  }
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
+      providers: [
+        {
+          provide: UsersService,
+          useValue: mockUsersService
+        }
+      ]
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
@@ -15,4 +48,10 @@ describe('UsersController', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  it('should return user profile', async () => {
+    const user = { first_name : 'test', last_name : 'user3', email : 'testuser3@gmail.com', username : 'testuser3', password : 'testpass123' }
+    const mockRequest = { body: user } as Request
+    expect( await controller.myProfile(mockRequest) ).toEqual( mockUsersService.myProfile(user) )
+  })
 });
