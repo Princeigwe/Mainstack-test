f7d9c35298e891221964ce8ffa6fd29d978250f0
Prince Igwe <igwep297@gmail.com>
2023-11-27 10:30:42 +0100

    mocked dependencies for services tests

--- src/products/products.service.spec.ts
+++ src/products/products.service.spec.ts
@@ -1,12 +1,32 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProductsService } from './products.service';
+import { UsersService } from '../users/users.service';
+import { getModelToken } from '@nestjs/mongoose';
+
 
 describe('ProductsService', () => {
   let service: ProductsService;
 
+  function mockProductModel(dto: any) {
+    this.data = dto
+    this.save = () => {return this.data}
+  }
+
+  let mockUsersService = {}
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ProductsService],
+      providers: [
+        ProductsService,
+        {
+          provide: getModelToken('Product'),
+          useValue: mockProductModel
+        },
+        {
+          provide: UsersService,
+          useValue: mockUsersService
+        }
+      ],
     }).compile();
 
     service = module.get<ProductsService>(ProductsService);
